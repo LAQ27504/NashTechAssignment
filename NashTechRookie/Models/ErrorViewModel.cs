e717d04e9ac42a4f80fde2e4b2f73f5855d11fda
LAQ27504 <anhquangcsp@gmail.com>
2025-04-03 09:31:12 +0700

    Initial assignment project

--- NashTechRookie/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace NashTechRookie.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
